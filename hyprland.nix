00dc709964ca68388ef53e2495c396c45e83bfe3
MikeTang <143688384+userjenney@users.noreply.github.com>
2025-06-06 13:15:04 +0800

    Update hyprland.nix

--- hyprland.nix
+++ hyprland.nix
@@ -7,7 +7,7 @@
     xwayland.enable = true;
     withUWSM = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
