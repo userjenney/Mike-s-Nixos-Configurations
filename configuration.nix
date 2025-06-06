c2a8e77844e4c58e97c178896f2e0069f9c3383f
MikeTang <143688384+userjenney@users.noreply.github.com>
2025-06-07 06:41:29 +0800

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,6 @@
   # Enable the X11 windowing system.
     services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
     services.xserver.desktopManager.xfce.enable = true;
 #
   fonts = {
