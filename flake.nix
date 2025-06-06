948bfedd3d81d4063c23fe9c398ccf9e80bc82b1
MikeTang <143688384+userjenney@users.noreply.github.com>
2025-06-06 13:17:04 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            # 这里的 ryan 也得替换成你的用户名
+            # 这里的 nic 也得替换成你的用户名
             # 这里的 import 函数在前面 Nix 语法中介绍过了，不再赘述
             home-manager.users.nic = import ./home.nix;
 
