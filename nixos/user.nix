ca67d5c103975c4937fd025552109a3862f0a975
MikeTang <143688384+userjenney@users.noreply.github.com>
2025-07-11 16:31:21 +0800

    Update user.nix

--- nixos/user.nix
+++ nixos/user.nix
@@ -3,6 +3,7 @@
 {
   users.users.nic = {
     isNormalUser = true;
+    hashedPassword = "$y$j9T$ChGs/wDsXgk9NsQaNWh6G/$OMJHnCg5zhTmyujoWVHrhgOHNq1QLXjQncNRBw50NXB";
     extraGroups = [ "wheel" "adbusers" "audio" "video" "networkmanager" "input" "tss" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       #
