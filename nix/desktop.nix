57b791a40ba6fe48f8b6e2aebbfcc132ccf744e5
MikeTang <143688384+userjenney@users.noreply.github.com>
2026-01-02 15:28:33 +0800

    Delete nix directory

--- nix/desktop.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ...}:
-
-{
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-}
--- nix/game.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-
-{
-  # steam program
-  programs.steam.enable = true;
-  programs.steam.extraCompatPackages = with pkgs; [ proton-ge-bin ];
-}
